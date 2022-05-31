1d6ca0c4575e9cd27be4c79b1b996e482a7be631
Mahmud2640 <mdmahmud484@gmail.com>
2022-06-01 02:18:45 +0600

    final

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,10 +1,82 @@
 import React from "react";
 import "./Portfolio.css";
+import image1 from "../../assets/portfolio1.jpg";
+import image2 from "../../assets/portfolio2.jpg";
+import image3 from "../../assets/portfolio3.jpg";
+import image4 from "../../assets/portfolio4.jpg";
+import image5 from "../../assets/portfolio5.png";
+import image6 from "../../assets/portfolio6.jpg";
 
 const Portfolio = () => {
+  // fake data do to use this in production
+  const data = [
+    {
+      id: 1,
+      image: image1,
+      title: "Portfolio 1 Use this line about your project.",
+      github: "https://github.com",
+      demo: "https://www.google.com",
+    },
+    {
+      id: 2,
+      image: image2,
+      title: "Portfolio 1 Use this line about your project.",
+      github: "https://github.com",
+      demo: "https://www.google.com",
+    },
+    {
+      id: 3,
+      image: image3,
+      title: "Portfolio 1 Use this line about your project.",
+      github: "https://github.com",
+      demo: "https://www.google.com",
+    },
+    {
+      id: 4,
+      image: image4,
+      title: "Portfolio 1 Use this line about your project.",
+      github: "https://github.com",
+      demo: "https://www.google.com",
+    },
+    {
+      id: 5,
+      image: image5,
+      title: "Portfolio 1 Use this line about your project.",
+      github: "https://github.com",
+      demo: "https://www.google.com",
+    },
+    {
+      id: 6,
+      image: image6,
+      title: "Portfolio 1 Use this line about your project.",
+      github: "https://github.com",
+      demo: "https://www.google.com",
+    },
+  ];
   return (
     <section id="portfolio">
-      <h1>Hello from Portfolio....</h1>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio_container">
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            <article key={id} className="portfolio_item">
+              <div className="portfolio_item-image">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio_item-cta">
+                <a href={github} className="btn" target="_blank">
+                  Github
+                </a>
+                <a href={demo} className="btn btn-primary" target="_blank">
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
+      </div>
     </section>
   );
 };
--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -1,10 +1,70 @@
 import React from "react";
+import { Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
 import "./Testimonials.css";
+import "swiper/css";
+import "swiper/css/pagination";
+import avatar1 from "../../assets/avatar1.jpg";
+import avatar2 from "../../assets/avatar2.jpg";
+import avatar3 from "../../assets/avatar3.jpg";
+import avatar4 from "../../assets/avatar4.jpg";
 
 const Testimonials = () => {
+  // fake data do not use production
+  const data = [
+    {
+      id: 1,
+      image: avatar1,
+      name: "Mahmud",
+      review:
+        " Lorem ipsum dolor sit amet consectetur adipisicing elit Similique itaque vitae illum dolores ratione minus autem atque! Nulla a, pariatur repudiandae esse animi natus minima inventore magnam doloremque illum. Esse.",
+    },
+    {
+      id: 2,
+      image: avatar2,
+      name: "Mahmud",
+      review:
+        " Lorem ipsum dolor sit amet consectetur adipisicing elit Similique itaque vitae illum dolores ratione minus autem atque! Nulla a, pariatur repudiandae esse animi natus minima inventore magnam doloremque illum. Esse.",
+    },
+    {
+      id: 3,
+      image: avatar3,
+      name: "Mahmud",
+      review:
+        " Lorem ipsum dolor sit amet consectetur adipisicing elit Similique itaque vitae illum dolores ratione minus autem atque! Nulla a, pariatur repudiandae esse animi natus minima inventore magnam doloremque illum. Esse.",
+    },
+    {
+      id: 4,
+      image: avatar4,
+      name: "Mahmud",
+      review:
+        " Lorem ipsum dolor sit amet consectetur adipisicing elit Similique itaque vitae illum dolores ratione minus autem atque! Nulla a, pariatur repudiandae esse animi natus minima inventore magnam doloremque illum. Esse.",
+    },
+  ];
   return (
-    <section id="Testimonials">
-      <h1>Hello from Testimonials....</h1>
+    <section id="testimonials">
+      <h5>Review from Clients</h5>
+      <h2>Testimonials</h2>
+
+      <Swiper
+        className="container testimonials_container"
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
+        {data.map(({ id, image, name, review }) => {
+          return (
+            <SwiperSlide key={id} className="testimonial">
+              <div className="client_avatar">
+                <img src={image} alt="profile" />
+              </div>
+              <h5 className="client_name">{name}</h5>
+              <small className="client_review">{review}</small>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </section>
   );
 };
