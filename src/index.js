b4de8d2300297bb9cb1b5bf7729eb039c7e17e67
Mahmud2640 <mdmahmud484@gmail.com>
2022-06-01 18:51:08 +0600

    added some feture

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+
 import App from "./App";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+root.render(<App />);
